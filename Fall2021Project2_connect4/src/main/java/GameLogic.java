a80e247e184323570a3b54e03472745e0f11be15
Bairez14 <Bairez14@users.noreply.github.com>
2021-10-25 01:02:49 -0500

    fixed diagobal methods
    Co-authored-by: xlzth <xlzth@users.noreply.github.com>

--- Fall2021Project2_connect4/src/main/java/GameLogic.java
+++ Fall2021Project2_connect4/src/main/java/GameLogic.java
@@ -12,7 +12,7 @@ public class GameLogic {
 		playerMoves = new ArrayList<GameButton>();
 	}
 
-	public void setPlayerMoves(ArrayList<GameButton> moves){
+	public void setPlayerMoves(ArrayList<GameButton> moves) {
 		this.playerMoves = moves;
 	}
 
@@ -34,7 +34,7 @@ public class GameLogic {
 				}
 				counter++;
 			}
-			System.out.println("Vertical win!!!");
+			//System.out.println("Vertical win!!!");
 			return true;
 		}
 	}
@@ -50,7 +50,7 @@ public class GameLogic {
 			}
 
 			if (counter == 4) {
-				System.out.println("Horizontal win!!!");
+				//System.out.println("Horizontal win!!!");
 				return true;
 			}
 		}
@@ -58,158 +58,129 @@ public class GameLogic {
 	}
 
 	public boolean winDiagonalUp(GameButton[][] buttons, GameButton b1) {
-		GameButton temp = b1;
+		int traverse = 1;
 		int counter = 0;
-		int p = b1.getPlayer(); // keeps track of player
-		GameButton start = diagonalHelper1(temp, p);
-		if (start.getRow() > 2) { // diagonal win can only happen when the row is greater than 2
+		int p = b1.getPlayer();
+		int r = b1.getRow();
+		int c = b1.getCol();
+		
+		// never be an instance where you can win diagonally in these coordinates
+		if ((c == 0 && r == 3) || (c == 0 && r == 4) || (c == 0 && r == 5) || (c == 1 && r == 4) || (c == 1 && r == 5)
+				|| (c == 2 && r == 5) || (c == 6 && r == 0) || (c == 6 && r == 1) || (c == 6 && r == 2)
+				|| (c == 5 && r == 0) || (c == 5 && r == 1) || (c == 4 && r == 0)) {
 			return false;
-		} //else {
-			if (buttons[start.getCol()][start.getRow()].getPlayer() == p) {
-				counter++;
+		}
+
+		// Check diagonally up since we would be starting at either col = 6 or row = 5
+		if (c == 6 || r == 5) {
+			for (int i = 0; i < 4; i++) {
+				if (buttons[c - i][r - i].getPlayer() != p) {
+					//System.out.println("First if");
+					//System.out.println("( " + (c + traverse) + ", " + (r - traverse) + ")");
+					//System.out.println("Player: " + b1.getPlayer());
+					return false;
+				}
 			}
-			while ((start.getRow() != 5) && (start.getCol() != 5)) {
-				// System.out.println("(" + start.getCol() + ", " + start.getRow() + ")");
-				start.setCol(start.getCol() + 1);
-				start.setRow(start.getRow() + 1);
-				if (buttons[start.getCol()][start.getRow()].getPlayer() == p) {
+			return true;
+		} else if (c == 0 || r == 0) { // Check diagonally down since we would be starting at either col = 6 or row = 0
+			for (int i = 0; i < 4; i++) {
+				if (buttons[c + i][r + i].getPlayer() != p) {
+					return false;
+				}
+			}
+			return true;
+		} else {
+			while ((c + traverse <= 6) && (r + traverse <= 5)) { // going down
+				if (buttons[c + traverse][r + traverse].getPlayer() == p) {
 					counter++;
-				} else {
-					counter = 0;
-				}
-				// System.out.println("counter: " + counter);
-				if (counter == 4) {
-					System.out.println("Diagonal Up win!!!");
+					traverse++;
+				} else if (counter >= 3) {
 					return true;
+				} else {
+					break;
 				}
 			}
-		//}
+			if (counter >= 3) {
+				return true;
+			}
+			//System.out.println("Counter: " + counter);
+			traverse = 1;
+			while ((c - traverse >= 0) && (r - traverse >= 0)) { // going up
+				if (buttons[c - traverse][r - traverse].getPlayer() == p) {
+					traverse++;
+					counter++;
+				} else if (counter >= 3) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			if (counter >= 3) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	public boolean winDiagonalDown(GameButton[][] buttons, GameButton b1) {
-		int counter = 0;
 		int traverse = 1;
+		int counter = 0;
 		int p = b1.getPlayer();
 		int r = b1.getRow();
 		int c = b1.getCol();
-		if (r < 5) {
-			traverse = 1; // resetting travers to check upwards
-			while (buttons[b1.getCol() + traverse][b1.getRow() - traverse].getPlayer() == p) {
-				counter++;
-				traverse++;
-			}
-			if (counter >= 3) {
-				return true;
-			}
-		} else {
-			//System.out.println("( " + (c - traverse) + ", " + (r + traverse) + ")");
-			// if the last move was top upper rightmost
-			while ((r + traverse <= 5) && (c - traverse <= 0)) {
-				if (buttons[b1.getCol() - traverse][b1.getRow() + traverse].getPlayer() == p) {
-					System.out.println("( " + (c - traverse) + ", " + (r + traverse) + ")");
-					counter++;
-					traverse++;
+		// never be an instance where you can win diagonally in these coordinates
+		if ((c == 0 && r == 0) || (c == 6 && r == 5) || (c == 6 && r == 4) || (c == 6 && r == 3) || (c == 5 && r == 5)
+				|| (c == 5 && r == 4) || (c == 4 && r == 5) || (c == 0 && r == 1) || (c == 0 && r == 2)
+				|| (c == 1 && r == 0) || (c == 1 && r == 1) || (c == 2 && r == 0)) {
+			return false;
+		}
+
+		// Check diagonally up since we would be starting at either col = 0 or row = 5
+		if (c == 0 || r == 5) {
+			for (int i = 0; i < 4; i++) {
+				if (buttons[c + i][r - i].getPlayer() != p) {
+					//System.out.println("First if");
+					//System.out.println("( " + (c + traverse) + ", " + (r - traverse) + ")");
+					//System.out.println("Player: " + b1.getPlayer());
+					return false;
 				}
 			}
-			System.out.println("counter: " + counter);
+			return true;
+		} else if (c == 6 || r == 0) { // Check diagonally down since we would be starting at either col = 6 or row = 0
+			for (int i = 0; i < 4; i++) {
+				if (buttons[c - i][r + i].getPlayer() != p) {
+					return false;
+				}
+			}
+			return true;
+		} else {
+			while ((c - traverse >= 0) && (r + traverse <= 5)) { // going down
+				if (buttons[c - traverse][r + traverse].getPlayer() == p) {
+					counter++;
+					traverse++;
+				} else if (counter >= 3) {
+					return true;
+				} else {
+					break;
+				}
+			}
 			if (counter >= 3) {
 				return true;
 			}
-
-			traverse = 1; // resetting travers to check upwards
-			while (buttons[b1.getCol() + traverse][b1.getRow() - traverse].getPlayer() == p) {
-				counter++;
-				traverse++;
+			//System.out.println("Counter: " + counter);
+			traverse = 1;
+			while ((c + traverse <= 6) && (r - traverse >= 0)) { // going up
+				if (buttons[c + traverse][r - traverse].getPlayer() == p) {
+					traverse++;
+					counter++;
+				} else if (counter >= 3) {
+					return true;
+				} else {
+					return false;
+				}
 			}
-			if (counter >= 3) {
-				return true;
-			}
-		}
-			// last move was left downward most
-			// if (counter == 0) {
-			// 	if (buttons[c + 1][r - 1].getPlayer() == p && buttons[c + 2][r - 2].getPlayer() == p 
-			// 			&& buttons[c + 3][r - 3].getPlayer() == p) {
-			// 		return true;
-			// 	}
-			//  }
-			// // had 1 button of type b1 under
-			// // we must now check 2 buttons above original
-			// if (counter == 1) {
-			// 	// the two right from original button whic is b1
-			// 	if (buttons[c + 1][r - 1].getPlayer() == p && buttons[c + 2][r - 2].getPlayer() == p) {
-			// 		return true;
-			// 	}
-			// }
-			// // had 2 buttons of type b1 under
-			// // check one above
-			// if (counter == 2) {
-			// 	if (buttons[c + 1][r - 1].getPlayer() == p) {
-			// 		return true;
-			// 	}
-			// }
-			
+		} 
 		return false;
-	}
-
-	/************************ METHOD 2  *************************************************/
-	// 	GameButton temp = b1;
-		
-	// 	int counter = 0;
-	// 	int traverse = 1;
-	// 	int p = b1.getPlayer(); // keeps track of player
-	// 	GameButton start = diagonalHelper2(temp, p);
-	// 	//System.out.println("(" + temp.getCol() + ", " + temp.getRow() + ")");
-
-	// 	if (start.getRow() > 2) { // diagonal win can only happen when the row is greater than 2
-	// 		return false;
-	// 	} // else {
-	// 		if (buttons[start.getCol()][start.getRow()].getPlayer() == p) { // checks first case
-	// 			counter++;
-	// 		}
-	// 		while ((start.getCol()-traverse != 1) && (start.getRow()+traverse != 5) && (start.getCol() != 0) && (start.getRow() != 0)) {
-	// 			System.out.println("(" + start.getCol() + ", " + start.getRow() + ")");
-	// 			// start.setCol(start.getCol() - 1);
-	// 			// start.setRow(start.getRow() + 1);
-
-	// 			if (buttons[start.getCol() - traverse][start.getRow() + traverse].getPlayer() == p) {
-	// 				traverse++;
-	// 				counter++;
-	// 			} else {
-	// 				counter = 0;
-	// 			}
-	// 			System.out.println("counter: " + counter);
-
-	// 			if (counter == 4) {
-	// 				System.out.println("Diagonal Down win!!!");
-	// 				return true;
-	// 			}
-	// 		}
-	// 		//}
-	// 		return false;
-		
-	// }
-
-	// helper function for winDiagonalUP
-	// Sets pointer to up left most diagonal place
-	public GameButton diagonalHelper1(GameButton temp, int p) {
-		while (temp.getPlayer() != p) {//temp.getPlayer() && temp.getRow() != 0
-			// System.out.println("(" + b1.getCol() + ", " + b1.getRow() + ")");
-			temp.setCol(temp.getCol() - 1);
-			temp.setRow(temp.getRow() - 1);
-		}
-		return temp;
-	}
-
-	// helper function for winDiagonalDown
-	// Sets pointer to up right most diagonal place
-	public GameButton diagonalHelper2(GameButton temp, int p) {
-		while (temp.getPlayer() != p) {
-			// System.out.println("(" + b1.getCol() + ", " + b1.getRow() + ")");
-			temp.setCol(temp.getCol() + 1);
-			temp.setRow(temp.getRow() - 1);
-		}
-		return temp;
 	}
 
 	public boolean gameTie(GameButton[][] buttons) {
@@ -220,22 +191,22 @@ public class GameLogic {
 				}
 			}
 		}
-		System.out.println("TIE!!!!!");
 		return true;
 	}
 
-	public void win(GameButton[][] buttons, GameButton b1) {
+	public boolean win(GameButton[][] buttons, GameButton b1, ObservableList<String> stats) {
 		if (winVertical(buttons, b1)
-				|| winHorizontal(buttons, b1)
-				|| winDiagonalDown(buttons, b1)
-			//	|| winDiagonalUp(buttons, b1)) {
-			//	|| gameTie(buttons)
-			) {
-			System.out.println("WINNER");
-		} else if (gameTie(buttons)){
-			System.out.println("Game Tie!!");
+			|| winHorizontal(buttons, b1) 
+		  	|| winDiagonalDown(buttons, b1)
+			|| winDiagonalUp(buttons, b1)) {
+			stats.add("Player " + b1.getPlayer() + " won!");
+			return true;
+		} else if (gameTie(buttons)) {
+			stats.add("Game is a tie!");
+			return false;
 		} else {
-			System.out.println("Keep going");
+			stats.add("Player " + b1.getPlayer() + "'s move: " + b1.getCol() + ", " + b1.getRow());
+			return false;
 		}
 	}
 
--- Fall2021Project2_connect4/src/main/java/JavaFXTemplate.java
+++ Fall2021Project2_connect4/src/main/java/JavaFXTemplate.java
@@ -1,25 +1,35 @@
+import java.time.Duration;
 import java.util.ArrayList;
-
+import java.util.HashMap;
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import java.time.*;
+import java.util.*;
+import java.time.Duration; //Time import
+import java.time.LocalDateTime;
 
 public class JavaFXTemplate extends Application {
 	private MenuBar menu;
 	public int count = 1;
 	GameLogic game = new GameLogic();
+	HashMap<String, Scene> sceneMap;
 	// 2D array that holds all the buttons
 	public GameButton[][] buttons = new GameButton[7][6];
 
@@ -31,16 +41,20 @@ public class JavaFXTemplate extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Welcome to Connect Four!");
+		//startingScene(primaryStage);
 		ObservableList<String> stats = FXCollections.observableArrayList();
-		ObservableList<String> turn = FXCollections.observableArrayList();
+		//ObservableList<String> turn = FXCollections.observableArrayList();
+		TextField turns = new TextField();
+		turns.setEditable(false);
 
 		// turnList and gameStatus
 		ListView<String> gameStats = new ListView<String>(stats);
-		ListView<String> turnList = new ListView<String>(turn);
-		turnList.setPrefWidth(500);
-		turnList.setPrefHeight(100);
-		turnList.setOrientation(Orientation.HORIZONTAL);
-		turn.add("Player 1's turn");
+		//ListView<String> turnList = new ListView<String>(turn);
+		// turnList.setPrefWidth(500);
+		// turnList.setPrefHeight(100);
+		// turnList.setOrientation(Orientation.HORIZONTAL);
+		turns.setAlignment(Pos.BOTTOM_CENTER);
+		turns.setText("Player 1's turn");
 
 		// Menu bar
 		menu = new MenuBar();
@@ -64,10 +78,10 @@ public class JavaFXTemplate extends Application {
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
-		addGrid(grid, turn, stats);
+		addGrid(grid, turns, stats);
 
 		// HBox for turnList
-		HBox turnBox = new HBox(turnList);
+		HBox turnBox = new HBox(turns);
 		turnBox.setAlignment(Pos.BOTTOM_CENTER);
 
 		// HBox
@@ -75,52 +89,72 @@ public class JavaFXTemplate extends Application {
 		layout.setAlignment(Pos.CENTER);
 		layout.setSpacing(50);
 
+		// creates the 3 themes and stores in a map
+		//sceneMap.put("theme1", setTheme1(layout, turn, stats));
+		//sceneMap.put("theme2", setTheme2(layout, turn, stats));
+		//sceneMap.put("theme3", setTheme3(layout, turn, stats));
+
+		// EventHandlers
+		iOne.setOnAction(e -> resetGame(turns, grid, stats));
+		iTwo.setOnAction(e -> System.exit(0));
+		iThree.setOnAction(e -> howToWindow());
+		reverse.setOnAction(e -> undo(game.getPlayerMoves(), turns, stats));
+		theme1.setOnAction(e-> primaryStage.setScene(sceneMap.get("theme1")));
+		theme2.setOnAction(e -> primaryStage.setScene(sceneMap.get("theme2")));
+		theme3.setOnAction(e -> primaryStage.setScene(sceneMap.get("theme3")));
+		
 		// Scene
 		Scene scene = new Scene(new VBox(40, menu, layout, turnBox), 1000, 1000);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-
-		// EventHandlers
-		iOne.setOnAction(e -> resetGame(turn, grid));
-		iTwo.setOnAction(e -> System.exit(0));
-		reverse.setOnAction(e -> undo(game.getPlayerMoves()));
-
 	}
 
-	public void resetGame(ObservableList<String> turn, GridPane grid) {
+	public void resetGame(TextField turns, GridPane grid, ObservableList<String> stats) {
 		for (Node n : grid.getChildren()) {
 			GameButton temp = (GameButton) n;
 			temp.setText("");
 			temp.setDisable(false);
 			temp.setStyle("-fx-background-color: lightBlue;" + "-fx-border-color: black;");
 		}
-		turn.clear();
-		turn.add("Player 1 goes first");
+		//turns.setText("");
+		stats.clear();
+		turns.setText("Player 1 goes first");
+
 		count = 1;
 	}
 
-	public void checkTurn(GridPane grid, ObservableList<String> turn, GameButton b1, ObservableList<String> stats) {
+	public void checkTurn(GridPane grid, TextField turns, GameButton b1, ObservableList<String> stats) {
 		// System.out.println("button pressed: " + ((Button)e.getSource()).getText());
 		if (count % 2 == 0) {
-			turn.add("Player 2's turn");
+			turns.setText("Player 1's turn");
 			b1.setPlayer(2);
 
 		} else {
-			turn.add("Player 1's turn");
+			turns.setText("Player 2's turn");
 			b1.setPlayer(1);
 		}
 		if (validMove(grid, b1, stats) == true) {
 			b1.setClicked(true);
 			b1.setDisable(true);
 			count++;
-			game.win(buttons, b1);
-			game.getPlayerMoves().add(b1);
+			game.win(buttons, b1, stats);
+			if(game.win(buttons, b1, stats)){ 	
+				Duration duration = Duration.ofSeconds(2);
+				PauseTransition pause = new PauseTransition(duration);
+				 // starts the pause
+				pause.setOnFinished(e -> winnerWindow(b1));
+				pause.play();
+				//winderWindow(b1);
+			}      
+			game.getPlayerMoves().add(b1); 
 
 		}
-		//b1.setDisable(validMove(grid, b1, stats));
 	}
 	
-	public GameButton undo(ArrayList<GameButton> playerMoves) {
+	public GameButton undo(ArrayList<GameButton> playerMoves,TextField turns, ObservableList<String> stats) {
+		GameButton b1 = playerMoves.get(playerMoves.size() - 1);
+		stats.add("Player " + b1.getPlayer() + " undid a move. Make a move!");
+		turns.setText("player " + b1.getPlayer() + "'s turn");
 		playerMoves.get(playerMoves.size() - 1).setDisable(false);
 		playerMoves.get(playerMoves.size() - 1).setClicked(false);
 		playerMoves.get(playerMoves.size() - 1).setPlayer(0);
@@ -153,34 +187,78 @@ public class JavaFXTemplate extends Application {
 		}
 	}
 
-	public void addGrid(GridPane grid, ObservableList<String> turn, ObservableList<String> stats) {
+	public void addGrid(GridPane grid, TextField turns, ObservableList<String> stats) {
 		for (int x = 0; x < 7; x++) { // column
 			for (int i = 0; i < 6; i++) { // row
 				GameButton b1 = new GameButton(x, i);
 				buttons[x][i] = b1; // populates buttons to 2d array of buttons
 				b1.setPrefSize(90, 90);
-				b1.setOnAction(e -> checkTurn(grid, turn, b1, stats));
+				b1.setOnAction(e -> checkTurn(grid, turns, b1, stats));
 				b1.setStyle("-fx-background-color: lightBlue;" + "-fx-border-color: black;");
 				grid.add(b1, x, i); // adding buttons to grid
 			}
 		}
 	}
 
-	public void setTheme1(Stage stage){
+	// public Scene setTheme1(HBox layout, ObservableList<String> turn, ObservableList<String> stats) {
+	// 	GridPane grid = new GridPane();
+	// 	grid.setAlignment(Pos.CENTER);
+	// 	grid.setHgap(10);
+	// 	grid.setVgap(10);
+	// 	addGrid(grid, turn, stats);
+	// 	return new Scene(layout, 1000, 1000);
+	// }
 
-	}
+	// public Scene setTheme2(HBox layout, ObservableList<String> turn, ObservableList<String> stats){
+	// 	GridPane grid = new GridPane();
+	// 	grid.setAlignment(Pos.CENTER);
+	// 	grid.setHgap(10);
+	// 	grid.setVgap(10);
+	// 	addGrid(grid, turn, stats);
+	// 	return new Scene(layout, 1000, 1000);
+	// }
 
-	public void setTheme2(Stage stage){
+	// //Original default theme
+	// public Scene setTheme3( ObservableList<String> turn, ObservableList<String> stats){ 
+	// 	return new Scene(layout, 1000, 1000);
+	// }
 
-	}
-
-	//Original default theme
-	public void setTheme3(Stage stage){ 
-
-	}
-
-	public void howToWindow(Stage stage){
+	// //Welcome scene
+	// public void startingScene(Scene primaryStage){ 
+	// 	//primaryStage.setTitle("Welcome to Connect Four1");
 		
+	// }
+	public void winnerWindow(GameButton b1) {
+		Stage newStage = new Stage();
+		VBox box = new VBox();
+		Button newGame = new Button("New Game");
+		Button exit = new Button("Exit");
+		TextField winner = new TextField("Player "+ b1.getPlayer()+ " won!");
+		//need to also list if it is a tie
+
+		
+		box.getChildren().add(winner);
+
+		Scene stageScene = new Scene(box, 600, 600);
+		newStage.setScene(stageScene);
+		newStage.show();
+	}
+
+	public void howToWindow(){
+		Stage newStage = new Stage();
+		newStage.setTitle("How to Play");
+		VBox box = new VBox();
+		TextField instructions = new TextField("Welcome to Cennoct Four!\n This is a two player game.\n The goal"
+			+ "is to try to build a row of four boxes while keeping your opponent from doing the same. Players will alternate "
+			+ "turns after selecting a button until a player selects 4 boxes in a row be in horixontally, vertical or diagonal. ");
+		
+		instructions.setFont(Font.font("Verdana", FontWeight.BOLD, 70));
+		instructions.setAlignment(Pos.CENTER);
+		box.getChildren().add(instructions);
+
+		Scene stageScene = new Scene(box, 600, 600);
+		newStage.setScene(stageScene);
+		newStage.show();
 	}
 
 }
